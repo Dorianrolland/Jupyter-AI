1fdc314cbe4dba7aea56101c3ddfe98ab5b1cc09
Dorianrolland <124084062+Dorianrolland@users.noreply.github.com>
2025-02-21 13:19:17 +0100

    Update start-jupyter2.sh

--- start-jupyter2.sh
+++ start-jupyter2.sh
@@ -7,19 +7,5 @@ ollama serve &
 sleep 5
 
 # Démarrer JupyterLab
-jupyter lab --ip=0.0.0.0 --port=8889 --no-browser \
-    --ServerApp.token='' \
-    --ServerApp.password='' \
-    --ServerApp.allow_origin='*' \
-    --ServerApp.disable_check_xsrf=True \
-    --ServerApp.trust_xheaders=True
-
-# Attendre que JupyterLab soit lancé
-sleep 5
-
-# Obtenir l'URL de JupyterLab
-url="http://localhost:8889/lab?token=$(jupyter server list | grep -oP '(?<=token=)[a-z0-9]{32}')"
-
-# Ouvrir l'URL dans le navigateur (sur l'hôte)
-xdg-open "$url" || echo "Ne peut pas ouvrir le navigateur, ouvrez $url manuellement"
-
+jupyter lab --ip=0.0.0.0 --port=8889 --allow-root \
+   
